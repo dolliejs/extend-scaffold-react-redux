cf3b50a2cc7e9b51dd9e1dbd3d55d3471b43fe6a
lenconda <i@lenconda.top>
2021-01-31 15:50:16 +0800

    initial commit

---
+++ .dollie.js
@@ -0,0 +1,28 @@
+module.exports = {
+  installers: [],
+  files: {
+    add: [
+      'src/store/(.*).(js|ts)',
+    ],
+    merge: [
+      'src/App.(js|tsx)',
+      'src/index.(js|tsx)',
+      'package.json',
+    ],
+  },
+  endScripts: [
+    (context) => {
+      if (context.fs.exists('tsconfig.json')) {
+        [
+          'src/store/models/app.js',
+          'src/store/index.js',
+        ].forEach((pathname) => context.fs.remove(pathname));
+      } else {
+        [
+          'src/store/models/app.ts',
+          'src/store/index.ts',
+        ].forEach((pathname) => context.fs.remove(pathname));
+      }
+    },
+  ],
+};
